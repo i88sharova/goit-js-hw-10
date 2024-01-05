1b17934d88e49829d6d45c9ec8c29a5d9a3d6b93
i88sharova <i88sharova@gmail.com>
2024-01-05 20:21:20 +0100

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -63,25 +63,25 @@ startBtn.addEventListener('click', () => {
 });
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
+  
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
+  
   const days = Math.floor(ms / day)
     .toString()
     .padStart(2, '0');
-  // Remaining hours
+  
   const hours = Math.floor((ms % day) / hour)
     .toString()
     .padStart(2, '0');
-  // Remaining minutes
+  
   const minutes = Math.floor(((ms % day) % hour) / minute)
     .toString()
     .padStart(2, '0');
-  // Remaining seconds
+  
   const seconds = Math.floor((((ms % day) % hour) % minute) / second)
     .toString()
     .padStart(2, '0');
